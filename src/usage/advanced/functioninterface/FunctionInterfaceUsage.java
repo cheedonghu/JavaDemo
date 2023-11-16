4dda66f58180aa1d49516c64bd08d286df2dce4a
luyublog <eastcukt@gmail.com>
2023-11-16 11:26:15 +0800

    function函数接口

---
+++ src/usage/advanced/functioninterface/FunctionInterfaceUsage.java
@@ -0,0 +1,20 @@
+package usage.advanced.functioninterface;
+
+/**
+ * 函数式接口使用
+ * function, supplier, consumer, predict
+ * <p>
+ * 1. 函数接口实例化方式：
+ * Lambda表达式、方法引用和匿名类。除此之外，还可以通过显式定义一个实现了 Function 接口的具体类来实例化。
+ * 2. 函数式接口优势：
+ * 延迟执行 条件执行
+ * <p>
+ *  todo 写篇博客
+ *  https://chat.openai.com/c/90bf09a3-275f-418a-aec0-f220dabbd4b6
+ *
+ * @author: east
+ * @date: 2023/11/16
+ */
+public class FunctionInterfaceUsage {
+
+}
