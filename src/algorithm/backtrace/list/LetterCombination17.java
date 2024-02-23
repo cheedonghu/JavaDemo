808fd2792c3d754135414af606d7f79edd72481c
luyublog <eastcukt@gmail.com>
2024-02-23 09:00:38 +0800

    回溯;17

---
+++ src/algorithm/backtrace/list/LetterCombination17.java
@@ -0,0 +1,37 @@
+package algorithm.backtrace.list;
+
+/**
+ * @author: east
+ * @date: 2024/1/24
+ */
+public class LetterCombination17 {
+//    public void backtrace(List<String> res, List<Character> state, List<List<Character>> choices,Integer n){
+//        if(isSolution(state,n)){
+//            recordSolution(res,state);
+//        }
+//
+//        for (List<Character> choice : choices) {
+//            for (Character c : choice) {
+//                if(isValid(state,res,n)){
+//                    updateState(state,c);
+//                    backtrace(res,state,choices,n);
+//                    return;
+//                }
+//            }
+//
+//        }
+//    }
+//
+//    /**
+//     * 判断是否合法
+//     * 1. state数量要小于n
+//     * 2.
+//     * @param state
+//     * @param res
+//     * @param n
+//     * @return
+//     */
+//    private boolean isValid(List<Character> state, List<String> res, Integer n) {
+//
+//    }
+}
